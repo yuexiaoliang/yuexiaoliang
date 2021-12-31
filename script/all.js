43d5bdc5f1efe29c758a1f704ea81a38cf3bcb34
岳晓亮 <2220124666@qq.com>
2021-12-31 11:13:15 +0800

    test

---
+++ script/all.js
@@ -0,0 +1,19 @@
+const locationEl = document.querySelector('.location');
+const successEl = document.querySelector('.success');
+const failEl = document.querySelector('.fail');
+
+showObj(locationEl, location);
+
+lightAppJssdk.user.getTicket({
+  success: function (data) {
+    showObj(successEl, data);
+  },
+  fail: function (data) {
+    showObj(failEl, data);
+  }
+});
+
+function showObj(el, obj) {
+  el.style.display = 'block';
+  el.innerHTML = JSON.stringify(obj, null, 2);
+}
