75eb6e5c54f07ae5f9571cf5ecfbf964ac55905b
岳晓亮 <2220124666@qq.com>
2021-12-31 14:20:08 +0800

    test

--- script/all.js
+++ script/all.js
@@ -1,21 +1,38 @@
 const locationEl = document.querySelector('.location');
 const successEl = document.querySelector('.success');
 const failEl = document.querySelector('.fail');
+const loginEl = document.querySelector('.login');
+const logoutEl = document.querySelector('.logout');
 
 showObj(locationEl, location);
+logoutEl.addEventListener('click', logout);
+loginEl.addEventListener('click', login);
 
-lightAppJssdk.user.getTicket({
-  success: function (data) {
-    if (data === '未登录') {
-      appLogin();
-      return;
+function login() {
+  lightAppJssdk.user.getTicket({
+    success: function (data) {
+      if (data === '未登录') {
+        appLogin();
+        return;
+      }
+      showObj(successEl, data);
+    },
+    fail: function (data) {
+      showObj(failEl, data);
     }
-    showObj(successEl, data);
-  },
-  fail: function (data) {
-    showObj(failEl, data);
-  }
-});
+  });
+}
+
+function logout() {
+  lightAppJssdk.user.logout({
+    success: function (data) {
+      alert(data);
+    },
+    fail: function (data) {
+      alert(data);
+    }
+  });
+}
 
 function appLogin() {
   lightAppJssdk.user.loginapp({
